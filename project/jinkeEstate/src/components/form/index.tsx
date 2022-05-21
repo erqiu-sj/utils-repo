4b523aecc3e86036c365aca736603d8ad2b2e0dd
剑潇潇 <5258675+booksword@user.noreply.gitee.com>
2022-05-21 06:47:08 +0000

    5月28日14:00-15:00

--- project/jinkeEstate/src/components/form/index.tsx
+++ project/jinkeEstate/src/components/form/index.tsx
@@ -71,7 +71,7 @@ const Form: FC<FormProps> = ({ saveed, formList, onSubmit }) => {
                 手机号: {curStateWithCommon.commonData.fans?.phone}
             </span>
             <span className='item'>
-                领奖时间: 5月28日—5月29日
+                领奖时间: 5月28日14:00-15:00
             </span>
             <span className='item'>
                 领奖地址: {curStateWithCommon.addr.address}
