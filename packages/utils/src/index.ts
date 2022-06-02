9d2379b1ee80f95c5d64e7b2205eaf922a995961
hooks_28 <1r56955415918@qq.com>
2022-06-02 17:38:44 +0800

    chore(all): change npm registy

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,14 +1,7 @@
 /*
  * @Author: 邱狮杰
  * @Date: 2022-05-21 23:19:06
- * @LastEditTime: 2022-05-21 23:19:06
- * @Description: 
- * @FilePath: /repo/packages/utils/src/index.ts
- */
-/*
- * @Author: 邱狮杰
- * @Date: 2022-05-18 22:13:28
- * @LastEditTime: 2022-05-18 22:56:09
+ * @LastEditTime: 2022-06-02 17:38:29
  * @Description: 
  * @FilePath: /repo/packages/utils/src/index.ts
  */
